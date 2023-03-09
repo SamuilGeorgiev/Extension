13fb327faafd1ac82c08b309940a94804ffb2272
Samuil <samuil0240@gmail.com>
2023-03-09 11:32:36 +0200

    render the leads in ul list using innerHTML

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ let myLeads = [];
 
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("inpul-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+//display leads from the array using innerHTML
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
