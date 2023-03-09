ce9619f2f648ff9598a4cbad290da2c6c1bffeda
Samuil <samuil0240@gmail.com>
2023-03-09 18:02:19 +0200

    Refactor the app using a string template

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 
 })
@@ -17,7 +18,13 @@ function renderLeads(){
     let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
     
-    listItems += "<li>" + myLeads[i] + "</li>"
+    listItems += `
+    <li>
+        <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+        </a>
+    </li>
+        `
 }
 ulEl.innerHTML = listItems
 }
